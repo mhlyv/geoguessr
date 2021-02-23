7f0672477de881d2c69c34092da1434675c4250f
Mihály Vesztergombi <mihaly.veszter@gmail.com>
2021-02-23 23:27:12 +0100

    it works!

--- mailbox/mailbox.go
+++ mailbox/mailbox.go
@@ -1,8 +1,9 @@
 package mailbox
 
 import (
-    // "encoding/json"
+    "encoding/json"
     // "errors"
+    "fmt"
     "io/ioutil"
     "net/http"
     "strings"
@@ -49,24 +50,78 @@ func Request(method string, url string, data string, header [][2]string) ([]byte
     return contents, nil;
 }
 
-func (m *MailBox) Init() error {
-    response, err := http.Get(API_BASE_URL + "?action=genRandomMailbox");
+func getBody(url string) ([]byte, error) {
+    response, err := http.Get(url);
     if err != nil {
-        return err;
+        return []byte{}, err;
     }
 
     defer response.Body.Close();
     raw, err := ioutil.ReadAll(response.Body);
     if err != nil {
+        return raw, err;
+    }
+
+    return raw, nil;
+}
+
+func (m *MailBox) Init() error {
+    raw, err := getBody(API_BASE_URL + "?action=genRandomMailbox");
+    if err != nil {
         return err;
     }
 
-    parts := strings.Split(string(raw[2:len(raw)-2]), "@");
-    m.login, m.domain = parts[0], parts[1];
+    /// this works but it's not right
+    // parts := strings.Split(string(raw[2:len(raw)-2]), "@");
+    // m.login, m.domain = parts[0], parts[1];
+
+    var addresses []string
+    err = json.Unmarshal(raw, &addresses);
+    if err != nil {
+        return err;
+    }
+
+    split := strings.Split(addresses[0], "@");
+    m.login, m.domain = split[0], split[1];
 
     return nil;
 }
 
-func (m *MailBox) Delete() error {
-    return nil
+func (m *MailBox) GetMessageIds() ([]int, error) {
+    ids := []int{};
+    raw, err := getBody(API_BASE_URL + "?action=getMessages" +
+        "&login=" + m.login + "&domain=" + m.domain);
+    if err != nil {
+        return ids, err;
+    }
+
+    var mails []map[string]interface{};
+    err = json.Unmarshal(raw, &mails);
+    if err != nil {
+        return ids, err;
+    }
+
+    for i := range mails {
+        ids = append(ids, int(mails[i]["id"].(float64)));
+    }
+
+    return ids, nil;
+}
+
+func (m *MailBox) ReadMessage(id int) (string, error) {
+    raw, err := getBody(API_BASE_URL + "?action=readMessage" +
+        "&login=" + m.login + "&domain=" + m.domain +
+        fmt.Sprintf("&id=%d", id));
+
+    if err != nil {
+        return string(raw), err;
+    }
+
+    var msg map[string]interface{};
+    err = json.Unmarshal(raw, &msg);
+    if err != nil {
+        return "", err;
+    }
+
+    return msg["body"].(string), nil;
 }
--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+    "errors"
     "fmt"
+    "log"
+    "net/http"
+    "regexp"
+    "time"
 
     "geoguessr/mailbox"
 )
+
+const PORT = 8080;
+
+// this is ugly but it has to be done, otherwise
+// the regex needs to be recompiled again and again
+var hrefPattern = func () *regexp.Regexp{
+    pattern, err := regexp.Compile("href=\\\"([^\"]+)\\\"");
+    if err != nil {
+        panic(err);
+    }
+    return pattern;
+}();
 
 func signUp(m *mailbox.MailBox) error {
     content, err := mailbox.Request(
@@ -28,7 +45,34 @@ func signUp(m *mailbox.MailBox) error {
     return nil;
 }
 
-func main() {
+func getVerificationId(m *mailbox.MailBox) (int, error) {
+    slept := time.Duration(0);
+    delay := 1 * time.Millisecond;
+    limit := 3 * time.Second;
+
+    for true {
+        time.Sleep(delay);
+        slept += delay;
+
+        ids, err := m.GetMessageIds();
+        if err != nil {
+            panic(err);
+        }
+
+        if len(ids) > 0 {
+            return ids[0], nil;
+        }
+
+        if slept > limit {
+            return 0, errors.New("email timed out");
+        }
+    }
+
+    panic("reached unreachable");
+    return 0, nil;
+}
+
+func getGeoguessrUrl() string {
     mail := &mailbox.MailBox{};
     err := mail.Init();
 
@@ -42,4 +86,30 @@ func main() {
     if err != nil {
         panic(err);
     }
+
+    id, err := getVerificationId(mail);
+    if err != nil {
+        panic(err);
+    }
+
+    msg, err := mail.ReadMessage(id);
+    if err != nil {
+        panic(err);
+    }
+
+    matched := hrefPattern.FindStringSubmatch(msg);
+    if len(matched) < 2 {
+        panic("regex failed");
+    }
+
+    return matched[1];
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, getGeoguessrUrl(), 301);
+}
+
+func main() {
+    http.HandleFunc("/", handler);
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil));
 }
